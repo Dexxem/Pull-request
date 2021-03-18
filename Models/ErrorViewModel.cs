ee12e2f24ba49dd4878440930f126cc67c248091
Dexxem <rsalolainen@gmail.com>
2021-03-18 11:09:24 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pull_request.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
